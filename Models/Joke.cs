2626b76dea7297cb39ddc9b98f009667c9038c11
Lorenzo <lorenzo1997chierisini@gmail.com>
2024-11-12 17:36:36 +0100

    added class Joke

---
+++ Models/Joke.cs
@@ -0,0 +1,18 @@
+﻿namespace JokesWebApp.Models
+{
+    // Classe Joke
+    public class Joke
+    {
+        //SETTER's & GETTER's
+        public int id { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+
+        // Costruttore
+        public Joke()
+        {
+            
+        }
+
+    }
+}
