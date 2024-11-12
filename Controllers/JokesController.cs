e033bac704af1f78175cad446268015e00ecd437
Lorenzo <lorenzo1997chierisini@gmail.com>
2024-11-12 18:10:29 +0100

    added comments on self generated JokesController to pin the code flow

--- Controllers/JokesController.cs
+++ Controllers/JokesController.cs
@@ -19,31 +19,42 @@ namespace JokesWebApp.Controllers
             _context = context;
         }
 
-        // GET: Jokes
+        // GET: Jokes       -- INDEX
         public async Task<IActionResult> Index()
         {
+            // Ritorna una vista (Index) con il metodo "await _context.Joke.ToListAsync()" che ottiene tutte le barzellete in modo asincrono.
             return View(await _context.Joke.ToListAsync());
         }
 
-        // GET: Jokes/Details/5
+
+        // GET: Jokes/Details/5     -- EDIT
+        // Edit riceve un id(parametro opzionale)
         public async Task<IActionResult> Details(int? id)
         {
+            // Condizione: Se l'id è nullo...
             if (id == null)
             {
+                // Ritorna "NotFound";
                 return NotFound();
             }
 
+            // Cerca joke con id specificato.
             var joke = await _context.Joke
                 .FirstOrDefaultAsync(m => m.id == id);
+            // Condizione: Se non lo trova...
             if (joke == null)
             {
+                // Ritorna null.
                 return NotFound();
             }
 
+            // Se trova l'id lo passa alla vista, altrimenti restituisce "NotFound".
             return View(joke);
         }
 
-        // GET: Jokes/Create
+        // GET: Jokes/Create        -- CREATE
+
+        // In GET cerca joke con id fornito e passa tutto alla vista
         public IActionResult Create()
         {
             return View();
@@ -52,20 +63,26 @@ namespace JokesWebApp.Controllers
         // POST: Jokes/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        // In POST riceve un oggetto Joke...
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("id,JokeQuestion,JokeAnswer")] Joke joke)
         {
+            // Condizione: Se i dati sono validi...
             if (ModelState.IsValid)
             {
+                // Aggiunge la barzelletta e salva...
                 _context.Add(joke);
                 await _context.SaveChangesAsync();
+                // Dopo avere creato l'oggetto, reinderizza alla vista.
                 return RedirectToAction(nameof(Index));
             }
             return View(joke);
         }
 
-        // GET: Jokes/Edit/5
+        // GET: Jokes/Edit/5    -- UPDATE
+
+        // In GET cerca joke con l'id fornito, se la trova, la passa alla vista per le modifiche...
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -84,6 +101,7 @@ namespace JokesWebApp.Controllers
         // POST: Jokes/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        // In POST...
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("id,JokeQuestion,JokeAnswer")] Joke joke)
@@ -93,13 +111,16 @@ namespace JokesWebApp.Controllers
                 return NotFound();
             }
 
+            // Verifica che l'id della joke sia valido.
             if (ModelState.IsValid)
             {
+                // Prova ad aggiornare la joke...
                 try
                 {
                     _context.Update(joke);
                     await _context.SaveChangesAsync();
                 }
+                // Se intercetta errore di concorrenza, verifica l'esistenza della joke.
                 catch (DbUpdateConcurrencyException)
                 {
                     if (!JokeExists(joke.id))
@@ -111,30 +132,36 @@ namespace JokesWebApp.Controllers
                         throw;
                     }
                 }
+                // Altrimenti reinderizza alla index.
                 return RedirectToAction(nameof(Index));
             }
+            // Restituisce la joke alla vista.
             return View(joke);
         }
 
-        // GET: Jokes/Delete/5
+        // GET: Jokes/Delete/5      -- DELETE
+
+        // Verifica se esiste la joke con l'id corrispondente...
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
             {
                 return NotFound();
             }
-
+            
             var joke = await _context.Joke
                 .FirstOrDefaultAsync(m => m.id == id);
             if (joke == null)
             {
                 return NotFound();
             }
-
+            // La passa alla vista per confermare la cancellazione...
             return View(joke);
         }
 
         // POST: Jokes/Delete/5
+
+        // In POST cerca la joke tramite id
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
@@ -142,10 +169,12 @@ namespace JokesWebApp.Controllers
             var joke = await _context.Joke.FindAsync(id);
             if (joke != null)
             {
+                // Se la trova la rimuove...
                 _context.Joke.Remove(joke);
             }
 
             await _context.SaveChangesAsync();
+            // Reinderizza alla vista index.
             return RedirectToAction(nameof(Index));
         }
 
